5f30784b1d5a45ffe687dd78e54a2e72c2a137cf
OddballGreg <ghavenga@e5r5p2.jnb.42.fr>
2016-09-27 16:22:33 +0200

    Further development of the Fact class. The prove method requires further development.

--- Fact.class.php
+++ Fact.class.php
@@ -7,7 +7,9 @@
 
 		static public $verbose = 1;
 		private $_name = NULL;
-		private $_dependencies = NULL;
+		private $_depend = NULL;
+		private $_anull = NULL;
+		private $_constant = FALSE;
 
 		/*Standard Basic Methods*/
 		
@@ -45,6 +47,42 @@
 				foreach ($facts as $fact)
 					print($fact . PHP_EOL);
 			}
+			if ($this->_constant == TRUE)
+			{
+				if (self::$verbose == TRUE)
+					print("Fact " . $this . "Proven TRUE as it is a Constant." . PHP_EOL);
+					return (TRUE);
+			}
+			else
+			{
+				$status = NULL;
+				foreach ($_anull as $rule)
+				{
+					$elems = explode(" ", $rule);
+					//Prove each element, then check following symbols to determine how to proceed
+				}
+			}
+		}
+
+		public function c_anull($string)
+		{
+			if (self::$verbose == TRUE)
+				print($string . "added to the anullment list of " . $this . PHP_EOPL);
+			$this->_anull[] = $string;
+		}
+
+		public function c_depend($string)
+		{
+			if (self::$verbose == TRUE)
+				print($string . "added to the dependency list of " . $this . PHP_EOPL);
+			$this->_depend[] = $string;
+		}
+
+		public function set_constant()
+		{
+			if (self::$verbose == TRUE)
+				print($this . "was made a constant and will always resolve to TRUE" . PHP_EOPL);
+			$this->_constant = TRUE;
 		}
 	}
 ?>
