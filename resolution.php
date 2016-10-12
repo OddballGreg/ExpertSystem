877d96cec59ce5489a67ac8eb8a2b2f618f54a8d
OddballGreg <ghavenga@e5r8p3.jnb.42.fr>
2016-10-12 10:21:25 +0200

    Resolutions able to split rules. Expression resolution is called from resolve_rule. Waiting for affectant resolution definition from BSaunders. Expression resolution able to handle bracket resolution recursively.

--- resolution.php
+++ resolution.php
@@ -14,26 +14,24 @@ function assign_prob_or($fact_list, $keys, $prob)
 
 function resolve_exp($fact_list, $expression)
 {
-	$chars = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-	//replace rule FACTS with their related probablities from $fact_list in the string itself
-	foreach ($chars as $fact)
+	do 
 	{
-		$pattern = "/" . $fact . "/";
-		$expression = preg_replace($pattern, sum($fact_list[$fact]) / count($fact_list[$fact]), $expression);
-	}
-	//use preg match to break the expression down into it's lower brackets
-	preg_match_all('/\(((?:[^()])*)\)/', $rule, $brackets);
+		//use preg match to break the expression down into it's lower brackets
+		unset($brackets);
+		preg_match_all('/\(((?:[^()])*)\)/', $rule, $brackets);
 
-	//solve the lower brackets and reinsert the results into the string
-	foreach ($brackets[1] as $single_exp)
-	{
-		while (contains($single_exp, "+"))
-		{
-			preg_match("/([^\+ ]* \+ [^\+ ]*)/", $single_exp, $solve);
-			
-		}
-	}
+		//solve the lower brackets and reinsert the results into the string
+		if ($brackets != NULL)
+			foreach ($brackets[1] as $lower_exp)
+			{
+				$result = resolve_exp($fact_list, $lower_exp);
+				$lower_exp = preg_quote($lower_exp, "/");
+				preg_replace($lower_exp, $result, $expression);
+			}
+	} while ($brackets != NULL);
 
+	$expression = str_replace(array('(',')'), '', $expression);
+	$or_sets = explode("|", $expression);
 
 	//resolve + operations, reinsert result
 
@@ -51,17 +49,32 @@ function resolve_exp($fact_list, $expression)
 function resolve_rule($fact_list, $rule)
 {
 	//split expression from affectants
+	$sides = explode("=>", $rule);
+
 	//create array for affectants dictating probablitiy allocation
+	$affectant = $sides[1];
+
+	//replace expression FACTS with their related probablities from $fact_list in the string itself
+	$expression = $sides[0];
+	$chars = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	foreach ($chars as $fact)
+	{
+		$pattern = "/\!" . $fact . "/";
+		if ($fact_list[$fact] !== TRUE)
+			$expression = preg_replace($pattern, (100 - sum($fact_list[$fact]) / count($fact_list[$fact])), $expression);
+		else
+			$expression = preg_replace($pattern, "0", $expression);
+		$pattern = "/" . $fact . "/";
+		if ($fact_list[$fact] !== TRUE)
+			$expression = preg_replace($pattern, sum($fact_list[$fact]) / count($fact_list[$fact]), $expression);
+		else
+			$expression = preg_replace($pattern, "100", $expression);
+	}
+
 	//call resolve_exp on expression
+	$probablility = resolve_exp($fact_list, $expression);
+	
 	//Allocate returned expression probablitiy to $fact_list according to the array created earlier
 }
 
 ?>
-
-( and ) which are fairly obvious. Example : A + (B | C) => D
-• ! which means NOT. Example : !B
-• + which means AND. Example : A + B
-• | which means OR. Example : A | B
-• ˆ which means XOR. Example : A ˆ B
-• => which means "implies". Example : A + B => C
-• <=> which means "if and only if". Example : A + B <=> C
