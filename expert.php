af141074d85fc8f9af3042d7077e80cb0187ba6b
Brian SAUNDERS <bsaunder@e5r5p5.jnb.42.fr>
2016-10-16 12:56:51 +0200

    pushage

--- expert.php
+++ expert.php
@@ -3,14 +3,10 @@
 require_once("libft_core.php");
 require_once("resolution.php");
 
-ini_set('display_errors', true);
-error_reporting(E_ALL);
-
 if ($argc < 0)
 	die ("Please input the name and path of the .txt file containing your rules, constants and query as an arguement.\n");
 else if ($argc > 2)
 	print ("This Expert System can only handle one arguement .txt file at a time.\n");
-
 if ($argc == 2)
 {
 	if ($argv[1] == NULL | !file_exists($argv[1]))
@@ -18,19 +14,11 @@ if ($argc == 2)
 		echo "Error: File does not exist\n";
 		exit(0);
 	}
-
-	//File read and parse
-
 	$file = file($argv[1]);
-	//print_r($file); //debug
-    $rhs_array;
-	$rule_array;
-	$constant_array;
-	$query_array;
 	foreach($file as $line)
 	{
-		preg_match("/([^#]+[<=> ]..+)#/", $line, $rule); // Will blatantly ignore the #symbol if vaild code is after it.
-		if ($rule == NULL)
+		preg_match("/([^#]+[<=> ]..+)#/", $line, $rule);
+        if ($rule == NULL)
 			preg_match("/(.+[<=> ]{2,3}[^\w].+)/", $line, $rule);
         if ($rule != NULL) {
 			$rule_array[] = $rule[1];
@@ -39,8 +27,8 @@ if ($argc == 2)
 	unset($line);
 	foreach($file as $line)
 	{
-		preg_match("/=([A-Z]+)/", $line, $constant); // Test for only '=' given to define constants
-		if ($constant != NULL)
+		preg_match("/=([A-Z]+)/", $line, $constant);
+        if ($constant != NULL)
 		{
 			$constant_a[] = explode(' ', $constant[1])[0];
 			$constant_array = str_split($constant_a[0]);
@@ -57,79 +45,40 @@ if ($argc == 2)
 		}
 	}
 	unset($line);
-    
 	$factsn = array();
 	$facts = array();
-
-	//below code will generate facts only according to those used in the defined rules
-	/*foreach($rule_array as $line)
-	{
-		preg_match_all("/([A-Z]{1})/", $line, $ufact);
-		$factsn = array_merge($factsn, $ufact[1]);
-		$factsn = array_unique($factsn);
-		foreach($factsn as $fact)
-		{
-			$facts[$fact] = array();
-		}
-		unset($fact);
-	}
-	unset($line);*/
-
-	//generate all possible facts
 	$chars = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	foreach ($chars as $char)
-	{
 		$facts[$char] = array();
-	}
-	//Define constants in a fact array as boolean true rather than arrays
 	foreach ($constant_array as $constant)
 		$facts[$constant] = TRUE;
 	unset($line);
 	echo "\nRules\n";
-	print_r($rule_array);
-	echo "Constants\n";
-	print_r($constant_array);
-	echo "Queries\n";
-	print_r($query_array);
-	echo "Facts\n";
-	print_r($facts);
-	echo "\n";
-
-    /******************** Splitting rhs *********************/
-    /*
-    $i = 0;
     foreach ($rule_array as $elem) {
-        echo ($elem . "\n");
-        $rule = substr($elem, strpos($elem, ">") + 1);
-        $rhs_array[$i] = $rule;
-        $i++;
+        echo $elem . "\n";
     }
-    
-    
-    print_r($rhs_array);
-    */
-    /************************ END ***************************/
-    
-	//Solving
-
+	echo "\nConstants\n";
+    foreach ($constant_array as $elem) {
+        echo $elem . "\n";
+    }
+	echo "\nQueries\n";
+    foreach ($query_array as $elem) {
+        echo $elem . "\n";
+    }
 	$rule_list = $rule_array;
 	$waiting_list = NULL;
     
-	while($rule_list != NULL) //Loop runs through the fact list infinitely to make sure all rules are used.
-	{
+	while($rule_list != NULL)
+    {
 		foreach ($rule_list as $rule)
 		{
-			if(contains($rule, "<=>")) //create singular sub rules from <=>
+			if(contains($rule, "<=>"))
 			{
 				$temp = explode('<=>', $rule);
 				$rule_list[] = trim($temp[1]) . ' => ' . trim($temp[0]);
                 $rule_list[] = trim($temp[0]) . ' => ' . trim($temp[1]);
-                echo "<=> subrules created" . PHP_EOL;
                 $tmp_key = array_search($rule, $rule_list);
-                echo "array search for $rule found $tmp_key" . PHP_EOL;
                 unset($rule_list[$tmp_key]);
-                echo "rule_list : ";
-                print_r($rule_list);
                 $push = FALSE;
 			}
 			else
@@ -139,41 +88,25 @@ if ($argc == 2)
 				$rhs = explode("=>", $rule)[1];
                	preg_match_all("/([A-Z])/", $lhs, $deps);
 				preg_match_all("/([A-Z])/", $rhs, $affs);
-                
-                /******************** TESTING PHASE *********************/
-                
-               // $dep_keys = array_combine($deps[1], $deps[1]);
-               // $aff_keys = array_combine($affs[1], $affs[1]);
-				echo "Affectants: ";
-				print_r($affs[1]);
-				echo "\nDepenendents: ";
-				print_r($deps[1]);
-				echo "\n";
-				echo ("lhs = " . $lhs . "\n" . "rhs = " . $rhs . "\n");
-            
                 if (strpos($rhs, "+") !== false) {
                     $rhs2 = str_replace("+", " ", $rhs);
                     $rhs2 = str_replace(" ", "", $rhs2);
                     $len = strlen($rhs2);
     
                 }
-                
-                /******************** END TESTING PHASE *********************/
-                
-				foreach ($rule_list as $rule_check) // Check for fact dependencies in remaining rules
+				foreach ($rule_list as $rule_check)
 				{
                     $tmp_key = array_search($rule, $rule_list);
-                    echo "array search for $rule found $tmp_key" . PHP_EOL;
                     if (array_search($rule_check, $rule_list) < $tmp_key + 1)
                         break;
 					foreach ($deps[1] as $dep)
 					{
 						$check = explode("=>", $rule_check)[1];
-						if (contains($check, trim($dep)) && $facts[trim($dep)] != TRUE) // Forces the rule to be pushed to the next list if it's listed dependencies have not been fully defined and are not constants
-							$push = TRUE; //Variable sets this rule to be pushed to the next list.
+						if (contains($check, trim($dep)) && $facts[trim($dep)] != TRUE)
+							$push = TRUE;
 					}
 				}
-				if ($waiting_list != NULL) // Check for fact dependencies in rules already pushed to the waiting list.
+				if ($waiting_list != NULL)
 				{
 					foreach ($waiting_list as $rule)
 					{
@@ -187,26 +120,19 @@ if ($argc == 2)
 				}
                 if ($push === TRUE)
                 {
-                    $waiting_list[] = $rule; // Pushes the rule to the waiting list for the next runthrough
+                    $waiting_list[] = $rule;
                     $push = FALSE;
                 }
-				else // Resolve the rule
+				else
 				{
 					$facts = resolve_rule($facts, $rule);
 				}
 			}
 			
         }
-        echo "Rule list :";
-		print_r($rule_list); // DEBUG
-        echo PHP_EOL . "Facts list :";
-		print_r($facts);
-        echo PHP_EOL . "Waiting list :";
-        print_r($waiting_list);
-        echo PHP_EOL;
 		$rule_list = $waiting_list;
     }
-
+    echo "\nQuery resolved:\n";
     foreach ($query_array as $query)
     {
         if ($facts[$query] === TRUE)
@@ -225,73 +151,5 @@ if ($argc == 2)
             echo "$query is FALSE" . PHP_EOL;
 
     }
-
-    
-    /*************************** old parse function ****************************/
-
-/*	$file = file_get_contents($argv[1]);
-	preg_match_all('/(.+?)[#$]/', $file, $info);
-	preg_match_all("/=> (.+?)#/", $file, $affectant_array);
-	preg_match_all("/(.+?)=>/", $file, $depend_array);
-	$values = implode(" ", $info[1]);;
-	preg_match_all("/[A-Z]/", $values, $newvalues);
-	$test = implode(" ", $newvalues[0]);
-	$test = count_chars( $test, 3);
-	preg_match_all("/[A-Z]/", $test, $newtest);
-	
-	$newarray = array_map('trim', $info[1]);
-	$newarray = array_filter($newarray);
-	$loop = TRUE;
-	while ($loop == TRUE) {
-		foreach ($newarray as $line) {
-			$i = 0;
-			if ($line[$i] === '?') {
-				$query = str_split($line, 1);
-			}
-			else if ($line[$i] === '=') {
-				$initial_facts = str_split($line, 1);
-			}
-		}
-		if ($query != null && $initial_facts != null) {
-			$loop = FALSE;
-		}
-	}
-	array_shift($newarray);
-	array_shift($query);
-	array_shift($initial_facts);
-	print_r($newarray); ////debuggery and other wizard shit to shows rules
-	print_r($query); ////debuggery and other wizard shit to shows the ?query
-	print_r($initial_facts); ////debuggery and other wizard shit to shows given true values
-	$unique_values = array_combine($newtest[0], $newtest[0]);
-	print_r($unique_values);
-	
-	echo "\n";//debuggery and other wizard shit
-	foreach ($initial_facts as $elem) {
-		$facts[$elem] = 100;
-	}
-	echo "\n"; //debuggery and other wizard shit
-
-	print_r($rule_array[1]);//debuggery and other wizard shit
-	print_r($depend_array[1]);//debuggery and other wizard shit
-	$counter = count($rule_array[1]);
-	$i = 0;
-	
-	while ($i < $counter)
-	{
-		$tmp = $rule_array[1][$i];
-		$tmp = trim($tmp);
-		$dep = $depend_array[1][$i];
-		echo "Affectant " . $tmp . "\n";
-		echo "Dependecy " .  $dep . "\n\n";
-		$i++;
-	}
-	echo ($tmp . "\n");
-
-	print_r($facts);
-	print_r($affectant_array);
-	print_r($depend_array);
- */
- /****************************** end of old parse function **************************/
-
 }
 ?>
--- resolution.php
+++ resolution.php
@@ -2,20 +2,16 @@
 
 function resolve_rhs($rhs) 
 {
-	echo("resolve rhs dealing with:" . $rhs . PHP_EOL);
 	preg_match_all("/([A-Z]{1})/", $rhs, $facts);
 	$facts = $facts[1];
 	foreach ($facts as $fact)
 		$dispersal[$fact] = 100;
 	$index = -1;
 	$bracket_sets = array();
-	echo("starting loop" . PHP_EOL);
 	do
 	{
-		echo("index = " . $index . "for" . $rhs . PHP_EOL);
 		$brackets = NULL;
 		preg_match_all('/\(((?:[^()])*)\)/', $rhs, $brackets);
-		print_r($brackets);
         $bracket_sets = array_merge($bracket_sets, $brackets[1]);
         if ($bracket_sets != NULL)
 		    while ($bracket_sets[++$index] != NULL)
@@ -24,7 +20,6 @@ function resolve_rhs($rhs)
 	if (strlen(trim($rhs)) == 1)
 		$dispersal[trim($rhs)] = 100;
 	$plus_sets = explode("+", $rhs);
-	echo("exploded on +" . PHP_EOL);
 	foreach ($plus_sets as $set)
 	{
 		if (strlen(trim($set)) == 1)
@@ -38,7 +33,6 @@ function resolve_rhs($rhs)
             }
         }
 	}
-	echo("dealing with brackets" . PHP_EOL);
     $index = -1;
     if ($bracket_sets != NULL)
     {
@@ -58,7 +52,6 @@ function resolve_rhs($rhs)
     		}
     	}
     }
-	echo("Returning Dispersal" . PHP_EOL);
 	return ($dispersal);
 }
 
@@ -66,31 +59,18 @@ function resolve_exp($facts, $expression)
 {
 	do 
 	{
-		//use preg match to break the expression down into it's lower brackets
 		unset($brackets);
 		preg_match_all('/\(((?:[^()])*)\)/', $expression, $brackets);
-
-		//solve the lower brackets and reinsert the results into the string
-	    print_r($brackets);
-            echo ("brackets count =" . count($brackets[0]) . PHP_EOL);
 		if (count($brackets[0]) != 0)
 			foreach ($brackets[1] as $lower_exp)
 			{
-                echo ("Lower expression before preg_quote and resolve_exp = " . $lower_exp . PHP_EOL);
 				$result = resolve_exp($facts, $lower_exp);
                 $lower_exp = "/\(" . preg_quote($lower_exp, "/") . "\)/";
-                echo ("Result = " . $result . PHP_EOL);
-                echo ("Lower expression = " . $lower_exp . PHP_EOL);
                 $expression = preg_replace($lower_exp, $result, $expression);
-                echo ($expression . PHP_EOL);
 			}
 	} while (count($brackets[0]) != 0);
-	echo("resolve_exp dowhile loop ended" . PHP_EOL);
 	$expression = str_replace(array('(',')'), '', $expression);
-
-	//solve or sets last by breaking on them first
 	$or_sets = explode("|", $expression);
-	//resolve + operations
 	foreach ($or_sets as $set)
 	{
 		$and_set = explode("+", $set);
@@ -110,26 +90,18 @@ function resolve_exp($facts, $expression)
 		$or_results[] = max($results);
 	}
 	$end_prob = (min($or_results) + array_sum($or_results) / count($or_results)) / 2;
-    echo ("End_prob before return: " . $end_prob . PHP_EOL);
 	return ($end_prob);
 }
 
 function resolve_rule($facts, $rule)
 {
-	//split expression from affectants
 	$sides = explode("=>", $rule);
-
-	//create array for affectants dictating probablitiy allocation
 	$affectant = $sides[1];
 	$allocation = resolve_rhs($affectant);
-
-	//replace expression FACTS with their related probablities from $facts in the string itself
 	$expression = $sides[0];
 	$chars = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-	echo("Replacing expression FACTS with related probs" . PHP_EOL);
 	foreach ($chars as $fact)
     {
-        echo ("Checking if $fact is in facts" . PHP_EOL);
         if (array_key_exists($fact, $facts))
 		{
 			$pattern = "/\!" . $fact . "/";
@@ -144,19 +116,10 @@ function resolve_rule($facts, $rule)
 				$expression = preg_replace($pattern, "100", $expression);
 		}
 	}
-	echo($expression . PHP_EOL);
-
-	//call resolve_exp on expression
-	echo("Resolving Expression" . PHP_EOL);
 	$probablility = resolve_exp($facts, $expression);
-
-	//Allocate returned expression probablitiy to $facts according to the array created earlier
-	echo("Allocating Probability" . PHP_EOL);
 	foreach ($chars as $fact)
 		if (array_key_exists($fact, $allocation))
             $facts[$fact][] = $probablility * ($allocation[$fact] / 100);
-
     return ($facts);
 }
-
 ?>
